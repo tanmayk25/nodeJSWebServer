16a801aaeb708dabb6c70d0c21b07884a4ed026a
tanmayk25 <tanmay7khandelwal@gmail.com>
2022-07-12 13:10:20 -0700

    Distributed System Assignment 1 final

---
+++ Readme.txt
@@ -0,0 +1,31 @@
+﻿README
+Name: 
+Tanmay Khandelwal
+Assignment Name: 
+Programming Assignment 1
+Date: 
+04/26/2022
+Description: 
+For this assignment, I have used node.js to create a web server that displays the index page of SCU’s website. I have incorporated an event driven architecture for each request that comes to the web server. After it processes the request, the connection gets terminated. The web server runs on 127:0.0.1:<port>. The webs server fetches the files from the directory specified. If the port and the directory isn’t specified then the default values are used. Default port is 8000 and the default directory is the directory inside which the app.js file is present. The web server runs indefinitely.
+Submitted Files:
+Instructions:
+
+
+To Execute the system must have Nodejs version && NPM (node package manager) 
+To install: $ sudo apt install nodejs npm
+
+
+Before running the web server you need to extract the content of the zip file name, “web”(check the script file to see what the directory should look like), into the directory of your choice.
+After extracting the directory you specified should contain the following items:
+1. Index.html
+2. SCU Login.html
+3. SCU Login_files (folder)
+4. Home - Santa Clara University_files (folder)
+After doing that there are two ways to execute the web server depending on where you extracted the files.
+        First you have to open the terminal and navigate to the directory where the server and app.js file is located.
+1. If you have extracted the file inside the same folder where the app.js and server file is present then you can simply write ./server
+2. If you have extracted it in a different directory then you need to type the command ./server -document_root "/home/moazzeni/webserver_files" -port 8888
+
+
+Other Information:
+The SCU logo and some other icons, at the bottom of the page, do not appear in google chrome, however they seem to appear in safari. I have mainly used safari for testing purposes.
---
+++ app.js
@@ -0,0 +1,131 @@
+//importing
+const http = require("http");
+const fs = require('fs');
+const url = require('url');
+
+const host = 'localhost';
+//sets the port to the one specified in the command line. If the port is not specified then assigns 8000 as default
+var port = process.argv[3] ? process.argv[3] : 8000;
+//sets the root directory path to the one specified in the command line. If the root directory path  is not specified then assigns the current working directory
+var dir =  process.argv[2] ? process.argv[2] : __dirname;
+console.log(__dirname);
+
+//setting up the header
+var header = {
+  'Content-Type': 'text',
+  'Date': (new Date()).toUTCString(),
+  'Connection': 'Keep-Alive',
+  'Keep-Alive': 'timeout=3',
+}
+
+//event executed for every request
+const requestListener = function (req, res) {
+    /*
+    res.writeHead is used to write the http header
+    res.write is used to send the contents of the request file
+    res.end is used to end the connection for the given request.
+    */
+    if(req.method == "GET" || req.method == "HEAD") { 
+        var path = req.url;
+        console.log(decodeURI(path));
+        var decoded_path = decodeURI(path);
+        //sets the content length of the header
+        // var file_size = getFileSize(decoded_path) ? getFileSize(decoded_path) : 512;
+        // header['Content-Length'] = file_size;
+        switch (decoded_path){
+            case '/':
+                header["Content-Type"] = "text/html";
+                res.writeHead(200, header);
+                res.write(fs.readFileSync(dir + "/index.html", 'utf8'));
+                res.end();
+                break;
+            case '/app':
+                res.writeHead(302, { Location: '/login' }).end(); 
+                break;
+            case '/login':
+                header["Content-Type"] = "text/html";
+                res.writeHead(200, header);
+                res.write(fs.readFileSync(dir + "/SCU Login.html", 'utf8'));
+                res.end();
+                break;
+            case '/admin':
+                header["Content-Type"] = "text/html";
+                res.writeHead(403, header);
+                res.end(`<html><body><h1>Error 403</h1><p>You do not have the permission to visit this file</p></body></html>`);
+                break;
+            case '/~':
+            case '/*':
+                header["Content-Type"] = "text/html";
+                res.writeHead(400, header);
+                res.end(`<html><body><h1>Error 400 Bad Request</h1><p>Your browser sent a request that the server couldn't understand</p></body></html>`);
+                break;
+            case decoded_path:
+                if (fs.existsSync(dir + decoded_path)) {
+                    var extension = decoded_path.split('.')
+                    if(extension[extension.length-1] == "css"){
+                        header["Content-Type"] = "text/css";
+                        res.writeHead(200, header);
+                        res.write(fs.readFileSync(dir + decoded_path));
+                        res.end();
+                    
+                    }
+                    else if(extension[extension.length-1] == "js"){
+                        header["Content-Type"] = "text/javascript";
+                        res.writeHead(200, header);
+                        res.write(fs.readFileSync(dir + decoded_path));
+                        res.end();
+                    
+                    }
+                    else if(extension[extension.length-1] == "png"){
+                        header["Content-Type"] = "image/png";
+                        res.writeHead(200, header);
+                        res.write(fs.readFileSync(dir + decoded_path));
+                        res.end();
+                    
+                    }
+                    else if(extension[extension.length-1] == "jpg" || extension[extension.length-1] == "jpeg"){
+                        header["Content-Type"] = "image/jpeg";
+                        res.writeHead(200, header);
+                        res.write(fs.readFileSync(dir + decoded_path));
+                        res.end();
+                    }
+                    else {
+                        res.writeHead(200, header);
+                        res.write(fs.readFileSync(dir + decoded_path));
+                        res.end();
+                    }
+                break;
+                }
+                else {
+                    header["Content-Type"] = "text/html";
+                    res.writeHead(404, header);
+                    res.end(`<html><body><h1>Error 404</h1><p>Page not found</p></body></html>`)  
+                    break;
+                }
+                default:
+                    break;
+            }
+    }
+    else {
+        header["Content-Type"] = "text/html";
+        console.log(req.method + " is not allowed");
+        res.writeHead(405, header);
+        res.end(`<html><body><h1>Error 405</h1><p>Method not allowed</p></body></html>`);
+    }
+};
+//gets the size of the request file
+ function getFileSize(path){
+    try {
+        const stats = fs.statSync(path);
+        return stats.size;
+    } 
+    catch (err) {
+    console.log(err);
+}
+ }
+ //creating the server
+const server = http.createServer(requestListener);
+//starting the server
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
